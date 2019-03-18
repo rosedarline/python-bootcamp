901c3d1ab9529ac9e23a0ede7054fbd0d374501e
Rose Adam <44856104+rosedarline@users.noreply.github.com>
2019-03-18 13:43:05 -0500

    update return.py

--- return.py
+++ return.py
@@ -1,15 +1,15 @@
-# def print_square_of_7(): #This function does not return anything
-# 	print(7**2)
+def print_square_of_7(): #This function does not return anything
+ 	print(7**2)
 
-# print_square_of_7()
+print_square_of_7()
 
-# def square_of_7(): 
-# 	print("I AM BEFORE THE RETURN!")
-# 	return 7**2
-# 	print("I AM AFTER THE RETURN!")
+def square_of_7(): 
+ 	print("I AM BEFORE THE RETURN!")
+ 	return 7**2
+ 	print("I AM AFTER THE RETURN!")
 
-# result = square_of_7()
-# print(result)
+result = square_of_7()
+print(result)
 
 def say_hi():
 	return 'Hi!'

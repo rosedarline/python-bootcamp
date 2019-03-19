7c6920f69b0b1df88e5526f342cd48eb35d5772a
Rose Adam <rosedarline@gmail.com>
2019-03-19 12:45:11 -0500

    Update return.py

--- return.py
+++ return.py
@@ -1,15 +1,14 @@
-# def print_square_of_7(): #This function does not return anything
-# 	print(7**2)
+def print_square_of_7(): #This function does not return anything
+	print(7**2)
 
-# print_square_of_7()
+print_square_of_7()
 
-# def square_of_7(): 
-# 	print("I AM BEFORE THE RETURN!")
-# 	return 7**2
-# 	print("I AM AFTER THE RETURN!")
+def square_of_7(): 
+	print("I AM BEFORE THE RETURN!")
+	return 7**2
 
-# result = square_of_7()
-# print(result)
+result = square_of_7()
+print(result)
 
 def say_hi():
 	return 'Hi!'

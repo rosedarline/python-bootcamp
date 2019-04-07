459e5c9019ae18607f6156fab8f9760157b84da8
Rose Adam <rosedarline@gmail.com>
2019-04-07 14:00:22 -0500

    Add args-order.py

--- args-order.py
+++ args-order.py
@@ -1,13 +1,13 @@
-def display_info(a, b, *args, instructor="Colt", **kwargs):
+def display_info(a, b, *args, instructor="James", **kwargs):
   # return [a, b, args, instructor, kwargs]
   print(type(args))
 
-print(display_info(1, 2, 3, last_name="Steele", job="Instructor"))
+print(display_info(1, 2, 3, last_name="Stone", job="Instructor"))
 
 # a - 1
 # b - 2
 # args (3)
-# instructor - "Colt"
-# kwargs - {'last_name': "Steele", "job": "Instructor"}
+# instructor - "James"
+# kwargs - {'last_name': "Stone", "job": "Instructor"}
 
 [1, 2, (3,), 'James', {'last_name': 'Stone', 'job': 'Instructor'}]

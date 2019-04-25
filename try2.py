1fd1d81da326b7afa3ee59680f227bf99b43bf92
Rose Adam <rosedarline@gmail.com>
2019-04-25 16:37:59 -0400

    Add try2.py

---
+++ try2.py
@@ -0,0 +1,18 @@
+# try:
+# except:
+# else:
+# finally:
+
+
+while True:
+	try:
+		num = int(input("please enter a number: "))
+	except ValueError:
+		print("That's not a number!")
+	else:
+		print("Good job, you entered a number!")
+		break
+	finally:
+		print("RUNS NO MATTER WHAT!")
+print("REST OF GAME LOGIC RUNS!")
+
